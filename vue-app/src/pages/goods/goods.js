d716e7bc2b529d3015031213dcef554fd16d6017
xulei <939242551@qq.com>
2019-08-02 23:28:23 +0800

    添加sku

--- vue-app/src/pages/goods/goods.js
+++ vue-app/src/pages/goods/goods.js
@@ -8,6 +8,7 @@ import './goods_sku.css'
 import Vue from 'vue'
 import axios from 'axios'
 import qs from 'qs'
+import Slides from '../components/slides.vue'
 
 Vue.config.productionTip = false ;
 Vue.prototype.$http = axios
@@ -16,22 +17,65 @@ let {id} = qs.parse(location.search.substr(1))
 new Vue({
   el: '#app',
   data:{
+    id,
     lists: null,
     deals: null,
-    inactive: true
+    inactive: true,
+    bannerLists :null,
+    Actions : 0,
+    skuNumber: 1,
+    sku: false,
+    cart: false,
   },
   components: {
-
+    Slides
   },
   methods: { 
     getLists(){
       this.$http.get('http://rap2api.taobao.org/app/mock/7058/goods/details',{id})
         .then(rep =>{ 
           this.lists = rep.data.data
+          this.bannerLists = []
+          this.lists.imgs.forEach(element => {
+            this.bannerLists.push({
+              clickUrl:'',
+              img:element
+            })
+          });
         })
         .catch(err=>{
           console.log(err)
         })    
+    },
+    changeActive(val){
+      this.Actions = val
+      document.body.style.overflow = val ? 'hidden':'auto'
+      document.documentElement.style.overflow = val ? 'hidden':'auto'
+     // document.body.style.height = val ? '100%' :'auto'
+     // document.documentElement.style.height = val ? '100%' :'auto'
+    },
+    changeSku(val){
+      if(this.skuNumber || val !== -1){ 
+        this.skuNumber =  this.skuNumber + val
+        this.lists.remain = this.lists.remain - val
+      }
+    },
+    addCart(){
+      this.Actions = 0
+      if (this.skuNumber) {
+      this.$http.post('http://rap2api.taobao.org/app/mock/7058/cart/add',{id,number:this.skuNumber})
+      .then(rsp=>{
+        if(rsp.data.status===200){
+          this.cart = true
+          setTimeout(()=>{
+            this.sku = true
+          },1000)
+          setTimeout(()=>{
+            this.sku = false
+          },2000)
+        }
+      }) 
+      }
     },
     sales(){
     this.inactive = false
