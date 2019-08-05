c2b322d64a933a50707ef4755976ae591964615f
xulei <939242551@qq.com>
2019-08-05 11:45:46 +0800

    完成

--- vue-app/src/pages/member/components/from.vue
+++ vue-app/src/pages/member/components/from.vue
@@ -17,19 +17,24 @@
           <div class="block-item">
             <label>选择地区</label>
             <div class="select-group">
-              <select class="js-province-selector">
+              <select class="js-province-selector" v-model="provinceValue">
                 <option value="-1">选择省份</option>
-                <option value="110000">北京市</option>
+                <option :value="p.value" v-for="p in addressData.list"
+                >{{p.label}}</option>
                
               </select>
-              <select class="js-city-selector">
+              <select class="js-city-selector" v-model="cityValue">
                 <option value="-1">选择城市</option>
-                <option value="440100">广州市</option>
+                <option :value="c.value" v-for="c in cityList"
+            
+                >{{c.label}}</option>
                
               </select>
-              <select class="js-county-selector" name="area_code" data-code="440402">
+              <select class="js-county-selector" name="area_code" v-model="districtValue">
                 <option value="-1">选择地区</option>
-                <option value="440402">香洲区</option>
+                <option :value="d.value" v-for="d in districtList"
+         
+                >{{d.label}}</option>
               </select>
             </div>
           </div>
@@ -39,13 +44,13 @@
           </div>
         </div>
       </div>
-      <div class="block section js-save block-control-btn">
+      <div @click="add" class="block section js-save block-control-btn">
         <div class="block-item c-blue center">保存</div>
       </div>
-      <div v-show="type==='edit'" class="block section js-delete block-control-btn">
+      <div v-show="type==='edit'" @click="removeAddress" class="block section js-delete block-control-btn">
         <div class="block-item c-red center">删除</div>
       </div>
-      <div v-show="type==='edit'" class="block stick-bottom-row center js-save-default">
+      <div @click="setDefault" class="block stick-bottom-row center js-save-default">
         <button class="btn btn-standard js-save-default-btn">设为默认收货地址</button>
       </div>
     </div>
@@ -53,21 +58,104 @@
 </template>
 
 <script>
+ import addressData from '../../../modules/js/address.json';
 
 export default {
   data() {
     return {
       address: "",
-      cityValue: "",
-      districtValue: "", 
+      cityValue: -1,
+      districtValue: -1, 
       id: '',
-      isDefault: null,
       name: "",
-      provinceValue: '',   
+      provinceValue: -1,   
       tel: '',
       type : this.$route.params.type,
-      list : this.$route.params.addressList
+      list : this.$route.params.addressList,
+      addressData,
+      cityList: null,
+      districtList : null,
     }
+  },
+  methods: {
+    add(){
+      let {name,id,tel,provinceValue,cityValue,districtValue,address} = this
+      let data = {name,id,tel,provinceValue,cityValue,districtValue,address}
+      if(this.type==="add"){
+        this.$http.post('http://rap2api.taobao.org/app/mock/7058/address/add',{data})
+        .then(rep=>{
+          this.$router.go(-1)
+        })
+        .catch(err=>{
+          console.log(err)
+          this.$router.go(-1)
+        }) 
+      }
+      if(this.type==="edit"){
+        this.$http.post('http://rap2api.taobao.org/app/mock/7058/address/updata',{data})
+        .then(rep=>{
+          this.$router.go(-1)
+        })
+        .catch(err=>{
+          console.log(err)
+          this.$router.go(-1)
+        }) 
+      }
+    },
+    setDefault(){
+      this.$http.post('http://rap2api.taobao.org/app/mock/7058/address/setDefault',{id:this.id})
+        .then(rep=>{
+          this.add()
+        })
+        .catch(err=>{
+          console.log(err)
+        }) 
+    },
+    removeAddress(){
+      this.$http.post('http://rap2api.taobao.org/app/mock/7058/address/remove',{id:this.id})
+        .then(rep=>{
+          this.$router.go(-1)
+        })
+        .catch(err=>{
+          console.log(err)
+          this.$router.go(-1)
+        }) 
+    }
+  },
+  created(){
+    if(this.type==='edit'){
+     this.address = this.list.address , 
+     this.id = this.list.id ,
+     this.name = this.list.name ,
+     this.provinceValue = parseInt(this.list.provinceValue) , 
+     this.tel = this.list.tel 
+    }
+  },
+  watch:{
+   provinceValue(val){
+     if(val === -1) return
+     let index = this.addressData.list.findIndex(item => {
+       return item.value = val
+     })
+     this.cityList = this.addressData.list[index].children
+     this.cityValue = -1
+     this.districtValue = -1
+      if(this.type==='edit'){
+       this.cityValue = parseInt(this.list.cityValue) 
+      }
+   },
+   cityValue(val){
+     if(val === -1) return
+     let index = this.cityList.findIndex(item => {
+       return item.value = val
+     })
+     this.districtList = this.cityList[index].children
+     this.districtValue = -1
+
+     if(this.type==='edit'){
+     this.districtValue = parseInt(this.list.districtValue)
+      }
+   },
   },
 }
 </script>
